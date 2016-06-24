6eab5e36f5afe135936b33ad5ca8b14856747154
VooDooBlooD <mdan.hagh@gmail.com>
2016-06-24 16:05:43 +0430

    add forms

---
+++ PostCompany/Forms/RegisterEmployeeForm.cs
@@ -0,0 +1,15 @@
+﻿using PostCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostCompany.Forms
+{
+	public class RegisterEmployeeForm
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public EmployeeRole Role { get; set; }
+	}
+}
