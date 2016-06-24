1680548d5ab9ee4e2c5eecaacdea9dc7229f60ae
VooDooBlooD <mdan.hagh@gmail.com>
2016-06-24 16:12:39 +0430

    add auth utils

---
+++ PostCompany/Utils/Authentication.cs
@@ -0,0 +1,28 @@
+﻿using PostCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostCompany.Utils
+{
+	public class Authentication
+	{
+		public static void Authenticate(int id, EmployeeRole role)
+		{
+			System.Web.HttpContext.Current.Session["id"] = id;
+			System.Web.HttpContext.Current.Session["role"] = role;
+			System.Web.HttpContext.Current.Session["type"] = UserType.Employee;
+		}
+		public static int GetCurrnetUserId()
+		{
+			return (int) System.Web.HttpContext.Current.Session["id"];
+		}
+	}
+
+	public enum UserType
+	{
+		Employee,
+		Customer
+	}
+}
---
+++ PostCompany/Utils/Authorize.cs
@@ -0,0 +1,22 @@
+﻿using PostCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostCompany.Utils
+{
+	public class Authorize
+	{
+		public static bool hasRole(EmployeeRole role)
+		{
+			if (System.Web.HttpContext.Current.Session["role"] == null)
+				return false;
+			if ((UserType) System.Web.HttpContext.Current.Session["type"] != UserType.Employee)
+				return false;
+			if ((EmployeeRole) System.Web.HttpContext.Current.Session["role"] != role)
+				return false;
+			return true;
+		}
+	}
+}
