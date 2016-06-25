7d7a84e243cee91c9e17ac16b719f61006966eff
VooDooBlooD <mdan.hagh@gmail.com>
2016-06-25 06:35:06 +0430

    add output forms

---
+++ PostCompany/OutputForms/CustomerProfileOForm.cs
@@ -0,0 +1,27 @@
+﻿using PostCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostCompany.OutputForms
+{
+	public class CustomerProfileOForm
+	{
+		public int Id { get; set; }
+		public string Username { get; set; }
+		public string Name { get; set; }
+		public string Phone { get; set; }
+		public string Address { get; set; }
+
+		public CustomerProfileOForm() { }
+		public CustomerProfileOForm(Customer c)
+		{
+			Id = c.Id;
+			Username = c.Username;
+			Name = c.Name;
+			Phone = c.Phone;
+			Address = c.Address;
+		}
+	}
+}
---
+++ PostCompany/OutputForms/EmployeeProfileOForm.cs
@@ -0,0 +1,25 @@
+﻿using PostCompany.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostCompany.Reports
+{
+	public class EmployeeProfileOForm
+	{
+		public int Id { get; set; }
+		public string Username { get; set; }
+		public string Name { get; set; }
+		public EmployeeRole Role { get; set; }
+
+		public EmployeeProfileOForm() { }
+		public EmployeeProfileOForm(Employee e)
+		{
+			Id = e.Id;
+			Username = e.Username;
+			Name = e.Name;
+			Role = e.Role;
+		}
+	}
+}
