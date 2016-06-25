7babc87cc18fe252ca4d8a3841c85f69b25e133b
VooDooBlooD <mdan.hagh@gmail.com>
2016-06-25 06:34:38 +0430

    add input forms

---
+++ PostCompany/InputForms/EditCustomerIForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostCompany.InputForms
+{
+	public class EditCustomerIForm
+	{
+		public string OldPassword { get; set; }
+		public string NewPassword { get; set; }
+		public string Name { get; set; }
+		public string Phone { get; set; }
+		public string Address { get; set; }
+	}
+}
---
+++ PostCompany/InputForms/RegisterCustomerIForm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PostCompany.InputForms
+{
+	public class RegisterCustomerIForm
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public string Name { get; set; }
+	}
+}
